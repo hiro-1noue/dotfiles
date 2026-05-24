022b19e40fdedd0b53f9b5bcd88ed70c97c36ab9
hiro <hiro.sec@outlook.jp>
2026-05-24 14:35:42 +0900

    remove hyprland's config

--- configuration.nix
+++ configuration.nix
@@ -60,143 +60,16 @@
     wofi
     waybar
     hyprland
+    fastfetch
+    cmatrix
     git
     wget
+    imv
     vim
+    neovim
+    eza
     firefox
     brightnessctl
   ];
 
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-
-  home-manager.users.hiro = { pkgs, ... }: {
-    home.stateVersion = "25.05";
-
-    home.file.".config/hypr/hyprland.conf".text = ''
-      monitor=,preferred,auto,auto
-
-      $terminal = kitty
-      $menu = wofi --show drun
-      $mainMod = SUPER
-
-      exec-once = waybar
-
-      env = XCURSOR_SIZE,24
-      env = HYPRCURSOR_SIZE,24
-
-      general {
-          gaps_in = 5
-          gaps_out = 20
-          border_size = 2
-          col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-          col.inactive_border = rgba(595959aa)
-          resize_on_border = false
-          allow_tearing = false
-          layout = dwindle
-      }
-
-      decoration {
-          rounding = 10
-          rounding_power = 2
-          active_opacity = 1.0
-          inactive_opacity = 1.0
-          shadow {
-              enabled = true
-              range = 4
-              render_power = 3
-              color = rgba(1a1a1aee)
-          }
-          blur {
-              enabled = true
-              size = 3
-              passes = 1
-              vibrancy = 0.1696
-          }
-      }
-
-      animations {
-          enabled = yes, please :)
-          bezier = easeOutQuint,0.23,1,0.32,1
-          bezier = almostLinear,0.5,0.5,0.75,1.0
-          bezier = quick,0.15,0,0.1,1
-          animation = global, 1, 10, default
-          animation = windows, 1, 4.79, easeOutQuint
-          animation = workspaces, 1, 1.94, almostLinear, fade
-      }
-
-      dwindle {
-          pseudotile = true
-          preserve_split = true
-      }
-
-      misc {
-          force_default_wallpaper = -1
-          disable_hyprland_logo = false
-      }
-
-      input {
-          kb_layout = us
-          follow_mouse = 1
-          sensitivity = 0
-          touchpad {
-              natural_scroll = false
-          }
-      }
-
-      gestures {
-          workspace_swipe = false
-      }
-
-      bind = $mainMod, Q, exec, $terminal
-      bind = $mainMod, C, killactive,
-      bind = $mainMod, M, exit,
-      bind = $mainMod, V, togglefloating,
-      bind = $mainMod, R, exec, $menu
-      bind = $mainMod, P, pseudo,
-      bind = $mainMod, J, togglesplit,
-      bind = $mainMod, D, exec, wofi --show drun
-      bind = $mainMod, left, movefocus, l
-      bind = $mainMod, right, movefocus, r
-      bind = $mainMod, up, movefocus, u
-      bind = $mainMod, down, movefocus, d
-      bind = $mainMod, 1, workspace, 1
-      bind = $mainMod, 2, workspace, 2
-      bind = $mainMod, 3, workspace, 3
-      bind = $mainMod, 4, workspace, 4
-      bind = $mainMod, 5, workspace, 5
-      bind = $mainMod, 6, workspace, 6
-      bind = $mainMod, 7, workspace, 7
-      bind = $mainMod, 8, workspace, 8
-      bind = $mainMod, 9, workspace, 9
-      bind = $mainMod, 0, workspace, 10
-      bind = $mainMod SHIFT, 1, movetoworkspace, 1
-      bind = $mainMod SHIFT, 2, movetoworkspace, 2
-      bind = $mainMod SHIFT, 3, movetoworkspace, 3
-      bind = $mainMod SHIFT, 4, movetoworkspace, 4
-      bind = $mainMod SHIFT, 5, movetoworkspace, 5
-      bind = $mainMod SHIFT, 6, movetoworkspace, 6
-      bind = $mainMod SHIFT, 7, movetoworkspace, 7
-      bind = $mainMod SHIFT, 8, movetoworkspace, 8
-      bind = $mainMod SHIFT, 9, movetoworkspace, 9
-      bind = $mainMod SHIFT, 0, movetoworkspace, 10
-      bind = $mainMod, S, togglespecialworkspace, magic
-      bind = $mainMod SHIFT, S, movetoworkspace, special:magic
-      bind = $mainMod, mouse_down, workspace, e+1
-      bind = $mainMod, mouse_up, workspace, e-1
-      bindm = $mainMod, mouse:272, movewindow
-      bindm = $mainMod, mouse:273, resizewindow
-      bindel = ,XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
-      bindel = ,XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-      bindel = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-      bindel = ,XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+
-      bindel = ,XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-
-
-      bindel = ,XF86MonBrightnessUp, exec, brightnessctl set +5%
-      bindel = ,XF86MonBrightnessDown, exec. brightnessctl set -5%
-
-      windowrule = suppressevent maximize, class:.*
-      windowrule = nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0
-    '';
-  };
 }
