0dede228dbaa5e49d1e53669ab33ea27928a05c1
hiro <hiro.sec@outlook.jp>
2026-05-24 11:59:25 +0900

    initial nixos config

---
+++ configuration.nix
@@ -0,0 +1,198 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Asia/Tokyo";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ja_JP.UTF-8";
+    LC_IDENTIFICATION = "ja_JP.UTF-8";
+    LC_MEASUREMENT = "ja_JP.UTF-8";
+    LC_MONETARY = "ja_JP.UTF-8";
+    LC_NAME = "ja_JP.UTF-8";
+    LC_NUMERIC = "ja_JP.UTF-8";
+    LC_PAPER = "ja_JP.UTF-8";
+    LC_TELEPHONE = "ja_JP.UTF-8";
+    LC_TIME = "ja_JP.UTF-8";
+  };
+
+  services.xserver.xkb = {
+    layout = "jp";
+    variant = "";
+  };
+
+  users.users.hiro = {
+    isNormalUser = true;
+    description = "hiro";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "25.05";
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  hardware.graphics.enable = true;
+
+  services.getty.autologinUser = "hiro";
+
+  programs.xwayland.enable = true;
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    noto-fonts-cjk-sans
+  ];
+
+  environment.systemPackages = with pkgs; [
+    kitty
+    wofi
+    waybar
+    hyprland
+    git
+    wget
+    vim
+    firefox
+  ];
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+
+  home-manager.users.hiro = { pkgs, ... }: {
+    home.stateVersion = "25.05";
+
+    home.file.".config/hypr/hyprland.conf".text = ''
+      monitor=,preferred,auto,auto
+
+      $terminal = kitty
+      $menu = wofi --show drun
+      $mainMod = SUPER
+
+      exec-once = waybar
+
+      env = XCURSOR_SIZE,24
+      env = HYPRCURSOR_SIZE,24
+
+      general {
+          gaps_in = 5
+          gaps_out = 20
+          border_size = 2
+          col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
+          col.inactive_border = rgba(595959aa)
+          resize_on_border = false
+          allow_tearing = false
+          layout = dwindle
+      }
+
+      decoration {
+          rounding = 10
+          rounding_power = 2
+          active_opacity = 1.0
+          inactive_opacity = 1.0
+          shadow {
+              enabled = true
+              range = 4
+              render_power = 3
+              color = rgba(1a1a1aee)
+          }
+          blur {
+              enabled = true
+              size = 3
+              passes = 1
+              vibrancy = 0.1696
+          }
+      }
+
+      animations {
+          enabled = yes, please :)
+          bezier = easeOutQuint,0.23,1,0.32,1
+          bezier = almostLinear,0.5,0.5,0.75,1.0
+          bezier = quick,0.15,0,0.1,1
+          animation = global, 1, 10, default
+          animation = windows, 1, 4.79, easeOutQuint
+          animation = workspaces, 1, 1.94, almostLinear, fade
+      }
+
+      dwindle {
+          pseudotile = true
+          preserve_split = true
+      }
+
+      misc {
+          force_default_wallpaper = -1
+          disable_hyprland_logo = false
+      }
+
+      input {
+          kb_layout = us
+          follow_mouse = 1
+          sensitivity = 0
+          touchpad {
+              natural_scroll = false
+          }
+      }
+
+      gestures {
+          workspace_swipe = false
+      }
+
+      bind = $mainMod, Q, exec, $terminal
+      bind = $mainMod, C, killactive,
+      bind = $mainMod, M, exit,
+      bind = $mainMod, V, togglefloating,
+      bind = $mainMod, R, exec, $menu
+      bind = $mainMod, P, pseudo,
+      bind = $mainMod, J, togglesplit,
+      bind = $mainMod, D, exec, wofi --show drun
+      bind = $mainMod, left, movefocus, l
+      bind = $mainMod, right, movefocus, r
+      bind = $mainMod, up, movefocus, u
+      bind = $mainMod, down, movefocus, d
+      bind = $mainMod, 1, workspace, 1
+      bind = $mainMod, 2, workspace, 2
+      bind = $mainMod, 3, workspace, 3
+      bind = $mainMod, 4, workspace, 4
+      bind = $mainMod, 5, workspace, 5
+      bind = $mainMod, 6, workspace, 6
+      bind = $mainMod, 7, workspace, 7
+      bind = $mainMod, 8, workspace, 8
+      bind = $mainMod, 9, workspace, 9
+      bind = $mainMod, 0, workspace, 10
+      bind = $mainMod SHIFT, 1, movetoworkspace, 1
+      bind = $mainMod SHIFT, 2, movetoworkspace, 2
+      bind = $mainMod SHIFT, 3, movetoworkspace, 3
+      bind = $mainMod SHIFT, 4, movetoworkspace, 4
+      bind = $mainMod SHIFT, 5, movetoworkspace, 5
+      bind = $mainMod SHIFT, 6, movetoworkspace, 6
+      bind = $mainMod SHIFT, 7, movetoworkspace, 7
+      bind = $mainMod SHIFT, 8, movetoworkspace, 8
+      bind = $mainMod SHIFT, 9, movetoworkspace, 9
+      bind = $mainMod SHIFT, 0, movetoworkspace, 10
+      bind = $mainMod, S, togglespecialworkspace, magic
+      bind = $mainMod SHIFT, S, movetoworkspace, special:magic
+      bind = $mainMod, mouse_down, workspace, e+1
+      bind = $mainMod, mouse_up, workspace, e-1
+      bindm = $mainMod, mouse:272, movewindow
+      bindm = $mainMod, mouse:273, resizewindow
+      bindel = ,XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
+      bindel = ,XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
+      bindel = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+      bindel = ,XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+
+      bindel = ,XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-
+
+      windowrule = suppressevent maximize, class:.*
+      windowrule = nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0
+    '';
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "hiro's NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
+
+  outputs = { self, nixpkgs, home-manager, hyprland, ... }:
+  {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        hyprland.nixosModules.default
+      ];
+    };
+  };
+}
