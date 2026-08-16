0b7352b6864ebe7d1bc770a9840241d71e142577
hiro <hiro.sec@outlook.jp>
2026-08-16 13:08:55 +0900

    update: Change config of input-method

--- home/modules/input-method.nix
+++ home/modules/input-method.nix
@@ -18,4 +18,65 @@
     [GroupOrder]
     0=Default
   '';
+
+  xdg.configFile."fcitx5/config".text = ''
+    [Hotkey]
+    EnumerateWithTriggerKeys=True
+    EnumerateForwardKeys=
+    EnumerateBackwardKeys=
+    EnumerateSkipFirst=False
+    ModifierOnlyKeyTimeout=250
+
+    [Hotkey/TriggerKeys]
+    0=Zenkaku_Hankaku
+
+    [Hotkey/ActivateKeys]
+    0=Henkan_Mode
+
+    [Hotkey/DeactivateKeys]
+    0=Muhenkan
+
+    [Hotkey/AltTriggerKeys]
+    0=Shift_L
+
+    [Hotkey/EnumerateGroupForwardKeys]
+    0=Super+space
+
+    [Hotkey/EnumerateGroupBackwardKeys]
+    0=Shift+Super+space
+
+    [Hotkey/PrevPage]
+    0=Up
+
+    [Hotkey/NextPage]
+    0=Down
+
+    [Hotkey/PrevCandidate]
+    0=Shift+Tab
+
+    [Hotkey/NextCandidate]
+    0=Tab
+
+    [Hotkey/TogglePreedit]
+    0=Control+Alt+P
+
+    [Behavior]
+    ActiveByDefault=False
+    resetStateWhenFocusIn=No
+    ShareInputState=No
+    PreeditEnabledByDefault=True
+    ShowInputMethodInformation=True
+    showInputMethodInformationWhenFocusIn=False
+    CompactInputMethodInformation=True
+    ShowFirstInputMethodInformation=True
+    DefaultPageSize=5
+    OverrideXkbOption=False
+    CustomXkbOption=
+    EnabledAddons=
+    DisabledAddons=
+    PreloadInputMethod=True
+    AllowInputMethodForPassword=False
+    ShowPreeditForPassword=False
+    AutoSavePeriod=30
+  '';
 }
